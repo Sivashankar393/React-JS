8ac9f621f3813eb06a873e429d42bd2ca4f1e150
Sivashankar393 <Sivashankar393@gmail.com>
2023-05-08 18:52:21 +0530

    ok

---
+++ Test 4/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function App() {
+    return (
+        <div>
+
+            <h1> React App - Component</h1>
+            <li> </li>  
+            <App/>
+        </div>
+    )
+}
+
+export default App
