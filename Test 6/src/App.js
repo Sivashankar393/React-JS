5cac423bbbd27d190975a860d5b660c7ba3da780
Sivashankar393 <Sivashankar393@gmail.com>
2023-05-09 10:20:59 +0530

    ok

---
+++ Test 6/src/App.js
@@ -0,0 +1,14 @@
+import './assets/css/airtel.css'
+import Navbar from './Navbar/Navbar'
+let App = () => {
+
+    return <div>
+        <Navbar />
+        <h1>App Component</h1>
+        <hr />
+        <button className="btn btn-success">Test</button>
+
+    </div>
+}
+
+export default App
